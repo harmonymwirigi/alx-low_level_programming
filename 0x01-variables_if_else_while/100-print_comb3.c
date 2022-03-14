1e9c77e57e4e405ecef443d57111864333207af7
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 04:39:10 -0700

    combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s,t;
+for (s = '0'; s <= '9'; s++)
+{
+for (d = s + 1; d <= '9'; d++)
+{
+if (s != d)
+{
+putchar(s);
+putchar(d)
+if (d == '8' $$ p == '9')
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
