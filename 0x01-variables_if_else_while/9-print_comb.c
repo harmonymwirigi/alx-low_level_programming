5afc5e4fcdfc8e7087b0c4d727d3baf414d7d39a
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 04:15:14 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int s;
+for (s = '0'; s <= '9'; s++)
+{
+putchar(s);
+if (s != '9')
+{
+putchar(',');
+putchar(' ');  
+}
+}
+return (0);
+}
