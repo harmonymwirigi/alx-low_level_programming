a544886d12b1f4d3e1448a2f5dc0a9abdd665072
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 01:44:23 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,15 +12,15 @@ int n, last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
-if(last_digit > 5)
+if (last_digit > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
-if(last_digit == 0)
+if (last_digit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-if(last_digit < 6 && last_digit != 0)
+if (last_digit < 6 && last_digit != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
