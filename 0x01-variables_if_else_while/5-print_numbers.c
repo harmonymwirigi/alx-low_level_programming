042a2368a2b6fc91fee02580b41ac84c4082805b
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 03:16:13 -0700

    from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int number,i;
+number = 10;
+for (i = 0; i < 10; 1 ++)
+{
+printf(i + "\n");
+}   
+return (0);
+}
