980c05225a5db8dc6cbabdeb076d01ea545816a1
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 05:09:04 -0700

    cm=ombination 4

---
+++ 0x01-variables_if_else_while/101_print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int a, b, c;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+for (c = b + 1; c <= '9'; c++)
+{
+  if ((a != b) != c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a == '7' && b == '8')
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
