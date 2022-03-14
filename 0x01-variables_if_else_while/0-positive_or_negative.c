ffd06bff499a94c0c2eff548ba56175b6bbc45b8
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 00:02:34 -0700

    is positve negative or xero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
 /**
  * Entry point level
  *
@@ -14,17 +12,17 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if( n=0 )
+if( n==0 )
    {
-     printf("is zero\n");
+     printf("%d is zero\n", n);
    }
 if( n>0 )
    {
-     printf("is positive\n");
+     printf("%d is positive\n", n);
    }
 if( n<0 )
    {
-     printf("is negative\n");
+     printf("%d is negative\n", n);
    }
 return(0);
 }
