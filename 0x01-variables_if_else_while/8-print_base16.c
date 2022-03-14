653ef48e748622bc310bda69954e67aa44b0d222
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-14 04:04:00 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+char ch;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n')
+return(0);
+}
