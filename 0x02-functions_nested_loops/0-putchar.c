5594f662cbe2aa7d55405039320edd86de05968a
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-18 04:19:47 -0700

    main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-char putcharacter[9] = "_putchar";
-int i;
-for(i = 0; 1 < 8; i++)
-{
-_putchar(putcharacter[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
