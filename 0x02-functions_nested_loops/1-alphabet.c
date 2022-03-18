c766016b273c78bd4d2c20b98af8c2682a18e7a8
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-18 04:45:50 -0700

    added functiosn

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
-{
-char a;
-for(a = 'a';a < 'z';a++)
-{
-_putchar(a);
-}
-_putchar('\n');
-}
 int main(void)
 {
 print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
 int _putchar(char c);
+void print_alphabet(void)
+{
+  char a;
+  for(a = 'a'; a <= 'z'; a++)
+    {
+      _putchar(a);
+    }
+  _putchar('\n');
+}
+
