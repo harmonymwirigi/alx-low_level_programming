a22de2e9e846726cce1d2d6a3b918462d9ba7141
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-18 05:06:11 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - check the code
+ * main - Entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
 int i;
-for(i = 97; i < 123; i++)
-  {
-   _putchar(i);
-  }
+for (i = 97;i < 123;i++)
+{
+_putchar(i);
+}
 _putchar('\n');
 }
