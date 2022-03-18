5c7bcdda0abd3b8e124f9704967e43777f65d986
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-18 05:40:22 -0700

    added x10 alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * Print alphabet_x10 - Print the alphabet 10 times
+ * print_alphabet_x10 - Print the alphabet 10 times
  *
  * Return: Always 0
  */
@@ -10,10 +10,10 @@ int i;
 int l;
 for (i = 0; i < 10; i++)
 {
-  for (l = 97; l < 123; l++)
-  {
-   _putchar(l);
-  }
-  _putchar('\n');
+for (l = 97; l < 123; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
 }
 }
