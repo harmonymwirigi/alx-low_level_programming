a7e836169022036948887e0c0b78de6c28d2213b
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-18 05:23:11 -0700

    added alphabet times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print alphabet - print 10 times the alphabet
+ *
+ * return void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int l;
+for (i = 0; i < 10; i++)
+{
+for (l = 97; l < 123; l++)
+{
+_putchar(l);	   
+}
+_putchar('\n');
+}
+}
