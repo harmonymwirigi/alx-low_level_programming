64eda8eca57629219db838e209f717a98e4882cd
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-19 00:44:06 -0700

    added lower and uper functions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - Show 1 if the input is a lowercase character
+ * Return: 1 ofr lowercase. 0 for the rest
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
