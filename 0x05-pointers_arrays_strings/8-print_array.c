68d0eabb68c1202c6423a0e045f30157e31eb746
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-22 05:47:44 -0700

    elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - this function n elements of an arrays of integers
+ * @a: an input array
+ * @n: number of element of the array
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+for (; i < n; i+)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
