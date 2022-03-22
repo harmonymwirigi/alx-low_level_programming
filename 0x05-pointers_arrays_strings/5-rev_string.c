0ed325251ab6989ccb3fd9d428d71bfe40a81cc7
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-22 02:45:04 -0700

    reversing string without print

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - functions that reverses
+ * @s: input string
+ * Return: nothing
+ */
+int rev_string(char *s)
+{
+int i, l;
+char a, z;
+l = _strlen(s);
+i = 0;
+while (i < l)
+{
+a = s[i];
+z = s[l];
+s[i++] = z;
+s[l--] = a;
+}
+}
