eee1f5391d4e88377b21d595cc713fc225aee77a
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-22 01:13:50 -0700

    swaping integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of intergers
+ * @a: input integer pointer
+ * @b: another input interger pointer
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+int harmo;
+harmo = *a;
+*a = *b;
+*c = harmo;
+}
+  
