3834114220d157dea3f18c02f281920a3e8ce615
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-22 06:10:36 -0700

    copy a string from source to destinations

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main,h"
+/**
+ * _strcpy - function that copies string pointedto buffer pointedto by dest
+ * @dest: destinations where the source will be copied
+ * @src: source string
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+char *n = *dest;
+while (*src)
+{
+*dest++ = *src++;
+}
+return (n);
+}
