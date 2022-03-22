f26a86449c7ebb50e1b5c80c97353b708b7758e4
harmonymwirigi <harmonymwirigi99@gmail.com>
2022-03-22 01:03:44 -0700

    added reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *reset_to_98 - updates the value to 98
- *@n - the integer input
+ * reset_to_98 - updates the value to 98
+ * @n: - the integer input pointer
  * Return: nothing
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
