b3b598e12ac671cd799c8d440ade411511eea855
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-03-24 09:26:06 +0300

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strcmp - function that compares two strings
+* @s1: input string one
+* @s2: input string two
+* Return: the defferent between two strings
+*/
+int _strcmp(char* s1, char* s2)
+{
+	int h, string1, string2, compare;
+	while (s1)
+	{
+		string1++;
+	}
+	while (s2)
+	{
+		string2++;
+	}
+	compare = string2 - string1;
+	return (compare);
+}
