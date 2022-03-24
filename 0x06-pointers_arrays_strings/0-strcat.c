6c66b391ff5300c5c197f61f39269222008fbc10
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-03-24 08:43:10 +0300

    concatenating strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _strcat - this function concatenates two strings 
+* @dest: - input destiny string one
+* @src: - input source strign
+* Return: concatenated character
+*/
+char* _strcat(char* dest, char* src)
+{
+	char* temp = dest;
+
+	while (*dest)
+		dest++;
+
+	while (*src)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
+}
