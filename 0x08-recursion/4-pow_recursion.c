c9c0a51e9c436f5f36e860a0dab51e48bc73c957
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-03-30 19:52:11 +0300

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x raised to the power of y
+ * @x: input interger
+ * @y: input interger
+ * Return:  value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x**y);
+}
