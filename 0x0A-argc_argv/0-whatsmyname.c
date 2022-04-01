d0fc59a7bf6dc0a7131f703257699bbe757d8a20
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-04-01 14:00:27 +0300

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function to return its name
+ * @argc: argument count
+ * @argv: argument count
+ * Return: nothing
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+argument in the main function
