35c4ca1606954e493f26ec7da0a1e06b19504b36
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-04-01 14:20:58 +0300

    my first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,11 @@
  * @argv: argument count
  * Return: nothing
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0])
+	}
 	return (0);
 }
