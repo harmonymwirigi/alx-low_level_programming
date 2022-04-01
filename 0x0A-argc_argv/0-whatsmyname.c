fd675c958e7dbf697256224e68b28942e5934ec5
harmonymwirigi <harmonymwirigi99@gmail.co>
2022-04-01 14:36:44 +0300

    first commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * main - the main function to return its name
- * @argc: argument count
- * @argv: argument count
- * Return: nothing
+ * main - a program that prints its name, followed by a new line
+ * @argc: arguement count
+ * @argv: argument value, a strings that come after calling function
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
-	{
 		printf("%s\n", argv[0]);
-	}
+
 	return (0);
 }
