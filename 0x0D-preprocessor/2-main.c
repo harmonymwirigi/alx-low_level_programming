5c3d8a1b6f1fa5e19d2709a5295ccf9781bd8188
harmonymwirigi <harmonymwirigi99.com>
2022-04-12 07:09:00 -0700

    more

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - prints the name of file
- *   * Return: Always 0 (Success)
- *    */
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+ **/
 int main(void)
 {
 	printf("%s\n", __FILE__);
